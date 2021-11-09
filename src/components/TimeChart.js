90ecd97a519046a44dc2bb52904d7bf6e77c067a
k20081kk <k20081kk@k20081kknoMacBook-Air.local>
2021-11-09 15:36:29 +0900

    fix: 記録開始時の時刻の変更ができないバグを修正

--- src/components/TimeChart.js
+++ src/components/TimeChart.js
@@ -133,7 +133,7 @@ const TimeChart = (props) => {
 
   const [chartData, setChartData] = useState([]);
   useEffect(() => {
-    axios.get(`https://kiroku-server.herokuapp.com/timeLog/timeChart/${dateString}`,
+    axios.get(`http://localhost:3001/timeLog/timeChart/${dateString}`,
       {
         headers: {accessToken: localStorage.getItem("accessToken")}
       }
